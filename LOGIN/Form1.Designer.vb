dae487c0d44f487c56d3ddf42650e490ce0d1365
MykiMorillo-03 <115846104+MykiMorillo-03@users.noreply.github.com>
2022-10-14 18:38:36 -0500

    Add files via upload

---
+++ LOGIN/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TxtUsuario = New System.Windows.Forms.TextBox()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtUsuario
+        '
+        Me.TxtUsuario.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TxtUsuario.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TxtUsuario.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TxtUsuario.ForeColor = System.Drawing.SystemColors.ScrollBar
+        Me.TxtUsuario.Location = New System.Drawing.Point(36, 253)
+        Me.TxtUsuario.Multiline = True
+        Me.TxtUsuario.Name = "TxtUsuario"
+        Me.TxtUsuario.Size = New System.Drawing.Size(262, 33)
+        Me.TxtUsuario.TabIndex = 1
+        Me.TxtUsuario.Text = "EMAIL"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TxtPassword.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TxtPassword.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TxtPassword.ForeColor = System.Drawing.SystemColors.ScrollBar
+        Me.TxtPassword.Location = New System.Drawing.Point(36, 330)
+        Me.TxtPassword.Multiline = True
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPassword.Size = New System.Drawing.Size(262, 33)
+        Me.TxtPassword.TabIndex = 2
+        Me.TxtPassword.Text = "Password"
+        '
+        'btnlogin
+        '
+        Me.btnlogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnlogin.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnlogin.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.btnlogin.Location = New System.Drawing.Point(93, 392)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(153, 39)
+        Me.btnlogin.TabIndex = 3
+        Me.btnlogin.Text = "Log In"
+        Me.btnlogin.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btnlogin.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.LOGIN.My.Resources.Resources._142bfcdd4c5c0f70a3ff3645149a0dee1
+        Me.ClientSize = New System.Drawing.Size(335, 473)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TxtUsuario)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtUsuario As TextBox
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents btnlogin As Button
+End Class
---
+++ LOGIN/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub login_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Dim Usuario As String
+        Dim Password As String
+
+        Usuario = TxtUsuario.Text
+        Password = TxtPassword.Text
+        If (Usuario = "miguel.morillop03@gmail.com") And (Password = "mykimorillo") Then
+            Form2.Show()
+
+        Else
+            MsgBox("Correo o contraseña incorrecta")
+
+        End If
+
+
+    End Sub
+End Class
